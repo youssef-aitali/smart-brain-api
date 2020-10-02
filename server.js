2e160b2e6194b32d0d2e7814ba388b0f06c19f4d
Youssef AIT ALI <youssef.aitali@outlook.com>
2020-10-02 07:13:50 +0100

    updating database host url

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-elliptical-86412',
     user : 'postgres',
-    password : '1994',
+    password : '',
     database : 'smartbrain'
   }
 });
