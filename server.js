56944f7b28a7fc5f2b67ee44a3184e30e59be31c
Youssef AIT ALI <youssef.aitali@outlook.com>
2020-10-02 07:17:57 +0100

    adding database connection environment variable

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-elliptical-86412',
-    user : 'postgres',
-    password : '',
-    database : 'smartbrain'
+    host : 'process.env.DATABASE_URL',
+    ssl: true,
   }
 });
 
