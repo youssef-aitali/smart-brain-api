805371024ddd3b8dead96f46834dd1043f079b45
Youssef AIT ALI <youssef.aitali@outlook.com>
2020-10-03 06:05:45 +0100

    fixing typo erro

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString : 'postgresql-elliptical-86412',
     ssl: true
   }
 });
